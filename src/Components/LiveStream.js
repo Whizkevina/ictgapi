32a0093cd06b43b5b718c5e0f7f1b3fa1eca9e9e
Okeoghene Eunice Igho <61965289+Igho-Okeoghene@users.noreply.github.com>
2022-05-27 08:53:57 +0100

    updates

--- src/Components/LiveStream.js
+++ src/Components/LiveStream.js
@@ -28,8 +28,8 @@ const LiveStream = () => {
        <Container fluid className="height center ">
             <ReactPlayer url={info.LiveStreamUrl} className='border' width='70%' height= '80%'/>
             </Container>
-            <p className='information'>Service Title: <span className='span'>{info.LiveStreamTitle}</span></p>
-            <p className='information'> Online Worshippers Count: <span className='span'>{info.OnlineUsersCount}</span></p>
+            <p className='information'>SERVICE TITLE: <span className='span'>{info.LiveStreamTitle}</span></p>
+            <p className='information'> ONLINE WORSHIPPERS COUNT: <span className='span'>{info.OnlineUsersCount}</span></p>
         
         <Footer />
        </div>
