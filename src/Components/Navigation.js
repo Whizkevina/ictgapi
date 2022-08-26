32f54f359eb8d8065157497ea615fcef6d111ed8
Aina Oluwatimilehin <wizkevinaaina@gmail.com>
2022-08-26 13:34:57 +0100

    added few code and made little adjustment.

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap';
+import React from "react";
+import logo from "./app_icon.png";
+import { Navbar, Nav, Container } from "react-bootstrap";
 const Navigation = () => {
-    return ( 
-        /*Navigation bar, bootstrap is in use,
+  return (
+    /*Navigation bar, bootstrap is in use,
         It contains direct links to the Home and Livestream page
         It is responsive for all devices */
-        <div>
-           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Container>
-            <Navbar.Brand>Living Faith Church Worldwide</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="me-auto">
-                <Nav.Link href="#">Home</Nav.Link>
-                <Nav.Link href="#livestream">LiveStream</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        </div>
-     );
-}
- 
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>
+            <img src={logo} alt="unit-logo" width="50" height="50" /> Living Faith Church Worldwide
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#livestream">LiveStream</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
 export default Navigation;
